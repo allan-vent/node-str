b6d11ab8a179ae32e6086a711a2ffab615d32883
Allan Ventura <allan7l2012@gmail.com>
2023-04-06 13:36:28 -0300

    first commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
+
+module.exports = {
+    connectionString: 'mongodb://balta:e296cd9f@localhost:27017/admin',
+    sendgridKey: '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',
+    containerConnectionString: 'SUA CONNECTION STRING'
+}
